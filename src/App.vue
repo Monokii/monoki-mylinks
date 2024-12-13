0eb70a73f3734dcf35a1d4b3171672d9aff7fc54
SpectralGlint <71020988+Monokii@users.noreply.github.com>
2024-12-14 04:12:39 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,41 @@
 <template>
-  <div style="height: auto; justify-content: space-between; gap: 30px;">
-    <div class="head">
-      <h1>모노키</h1>
-      <p>{{ dec }}</p>
-    </div>
-    <div style="display: flex; flex-wrap: wrap; justify-content: space-between; gap: 10px; margin-top: 10px;">
-      <div v-for="(item, i) in link" :key="i" class="linkbox">
-        <div>
-          <h3>{{ item.title }}</h3>
-          <p>{{ item.description }}</p>
-        </div>
-        <a :href="item.url" target="_blank">
-          <button>이동</button>
-        </a>
+  <div style="display: flex;">
+    <!--
+    <div class="sidebar">
+    사이드바 내용
+      <h2 style="margin: 5px 14px;">모노키</h2>
+      <a>홈</a>
+      <a>dkanrjask</a>
+    </div>  -->
+
+    <div class="page">
+      <div class="gnb">
+      <h3>모노키</h3>
+      <div style="display: flex;">
+        <a v-for="(gnb, i) in memu" :key="i">{{ gnb.title }}</a>
       </div>
     </div>
+    <div style="padding: 0px 8px; height: auto; justify-content: space-between; gap: 30px;">
+      <div class="head">
+        <h1>링크</h1>
+        <p>{{ dec }}</p>
+      </div>
+      <div style="display: flex; flex-wrap: wrap; justify-content: space-between; gap: 10px; margin-top: 10px;">
+        <div v-for="(item, i) in link" :key="i" class="linkbox">
+          <div>
+            <h3>{{ item.title }}</h3>
+            <p>{{ item.description }}</p>
+          </div>
+          <a :href="item.url" target="_blank">
+            <button>이동</button>
+          </a>
+        </div>
+      </div>
+    </div>
+    <a>v: {{ ver }}</a>
   </div>
-  <div style="margin: 20px 0px;">
-    <p>{{ ver }}</p>
-  </div>    
+  </div>
+
 </template>
 
 <script>
@@ -31,19 +48,23 @@ export default {
         {title: '우리.인생', description: '제가 주로 톳을 올리는 곳이죠.', url: "https://uri.life/@so"},
         {title: '스텔라.플레이스', description: '우리.인생이랑 둘 다 쓰다가 놔버린 곳이에요.', url: 'https://stella.place/@so'},
         {title: '버터스콘', description: '귀여워서 마음에는 들지만, 규칙이 저와는 안 맞아 보여요.', url: 'https://buttersc.one/@mono'},
-        {title: '표현의 모에', description: '발언의 자유와 개인정보를 중점 운영이지만, 쓸지는 모름.', url: 'https://feels.moe/@monoki'},
+        {title: '표현의 모에', description: '발언의 자유와 개인정보 중점 운영이지만, 쓸지는 모름.', url: 'https://feels.moe/@monoki'},
         {title: '칸노트', description: '계정 만들어 놓고 까먹은 곳이에요.', url: 'https://iqhina.org/@o'},
         {title: '블루스카이', description: '트위터 탈중앙화 버전인데, 잘 안 써요.', url: 'https://bsky.app/profile/yume.place'},
         {title: '깃허브', description: '그냥 저의 깃허브 프로필 이에요, 딱히 뭐 없어요.', url: 'https://github.com/Monokii'},
         {title: '치지직', description: '흠?', url: 'https://chzzk.naver.com/8ce3e9df8179191c4e412a20065fbe1a'},
         {title: '유튜브', description: '그냥 링크 걸려고 만들었어요.', url: 'https://www.youtube.com/@yume_place'},
-        {title: '니코니코(다시:돌아옴)', description: '왜 만든 걸까?', url: 'https://www.nicovideo.jp/user/136988492/video'},
+        {title: '니코니코(다시:돌아옴)', description: '아 nicopatty가 생각나요.', url: 'https://www.nicovideo.jp/user/136988492/video'},
         {title: '로블록스 프로필', description: '2017년 1월 부터 시작했어요, 넷카마 아니에요.', url: 'https://www.roblox.com/ko/users/3638705580/profile'},
         {title: '퀘스돈@플래닛', description: '질문 해보세요, 기다리고 있어요.', url: 'https://quesdon.planet.moe/@so@uri.life'},
         {title: '네오-퀘스돈', description: '질문 해보세요, 기다리고 있어요.', url: 'https://neo-quesdon.serafuku.moe/main/user/@so@uri.life'},
-        {title: '실험용', description: '내가 하고싶은 걸 시도해보고, 여러가지를 하는 곳.', url: 'https://yume.place'},
+        {title: '실험용1', description: '내가 하고싶은 걸 시도해보고, 여러가지를 하는 곳.', url: 'https://yume.place/'},
+        {title: '실험용2', description: '실험용 2임. 설명은 1과 같음.', url: 'https://sonnect.co/'},
       ],
-      ver : '12-11-2024add-1',
+      memu : [
+        {title: '링크'},
+      ],
+      ver : '12-14-2024-1',
     }
   },
 }
@@ -51,6 +72,68 @@ export default {
 
 <style scoped>
   @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css");
+  .sidebar {
+  width: 250px; /* 사이드바 너비 */
+  height: 100vh; /* 화면 전체 높이 */
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  background-color: var(--color-background-soft);
+  position: fixed; /* 화면 왼쪽에 고정 */
+  top: 0px;
+  left: 0px;
+  padding: 16px 10px;
+  box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
+  }
+  .sidebar a {
+    padding: 10px 14px;
+    transition: 0.2s;
+    cursor: pointer;
+    border-radius: 5px;
+  }
+  .sidebar a:hover {
+    padding: 10px 14px;
+    color: var(--color-background-rh);
+    background-color: var(--color-background-hover);
+  }
+  .page {
+    /* margin-left: 250px; */
+    padding: 0px 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+  }
+  .gnb {
+    display: flex;
+    background-color: var(--color-background-soft);
+    padding: 2px 20px;
+    border-radius: 10px;
+    align-items: center;
+    /* align-self: center;
+    align-self: start; */
+    gap: 14px;
+  }
+  .gnb a {
+    border-radius: 0px;
+    padding: 16px 16px;
+    border-radius: 40px;
+    transition: 0.32s;
+    cursor: pointer;
+  }
+  .gnb a:hover {
+    padding: 16px 20px;
+    border-radius: 5px;
+    color: var(--color-background-rh);
+    background-color: var(--color-background-hover);
+  }
+  .gnb a:active {
+    padding: 16px 20px;
+    color: var(--color-background-rh);
+    border-radius: 10px;
+    opacity: 0.8;
+    transform: scale(0.9);
+    /* border-bottom: 2px solid var(--color-background-rh); */
+  }
   .head {
     display: flex;
     flex-direction: column;
